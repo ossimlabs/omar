26e5db8fb796d9fabe0bbb7f5fe893ca4d8c716d
Garrett Potts <gpotts@radiantblue.com>
2015-10-21 19:25:53 -0400

    Added new setup file for install and setup of GRAILS, GROOVY and GRADLE

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -3,13 +3,14 @@ pushd `dirname $0` >/dev/null
 export SCRIPT_DIR=`pwd -P`
 pushd $SCRIPT_DIR/../.. >/dev/null
 export OMAR_DEV_HOME=$PWD
-pushd $OMAR_DEV_HOME/.. >/dev/null
-export OSSIM_DEV_HOME=$PWD
-export OSSIM_HOME=$OSSIM_DEV_HOME/ossim
-echo "@@@@@ OMAR_DEV_HOME=$OMAR_DEV_HOME"
-echo "@@@@@ OSSIM_DEV_HOME=$OSSIM_DEV_HOME"
-echo "@@@@@ OSSIM_HOME=$OSSIM_HOME"
-popd >/dev/null
+export OMAR_HOME=$OMAR_DEV_HOME/apps/omar
+#pushd $OMAR_DEV_HOME/.. >/dev/null
+#export OSSIM_DEV_HOME=$PWD
+#export OSSIM_HOME=$OSSIM_DEV_HOME/ossim
+#echo "@@@@@ OMAR_DEV_HOME=$OMAR_DEV_HOME"
+#echo "@@@@@ OSSIM_DEV_HOME=$OSSIM_DEV_HOME"
+#echo "@@@@@ OSSIM_HOME=$OSSIM_HOME"
+#popd >/dev/null
 popd >/dev/null
 
 #
---
+++ build_scripts/linux/setup.sh
@@ -0,0 +1,32 @@
+if [ ! -d "$HOME/.sdkman" ]; then
+   curl -s get.sdkman.io | bash
+fi
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
+if [ ! -z "$GROOVY_VERSION" ]; then
+   if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
+      sdk install groovy $GROOVY_VERSION
+   fi
+else
+   echo "******OMAR SETUP ERROR: Environment variable GROOVY_VERSION must be set"
+   exit 1
+fi
+
+if [ ! -z "$GRAILS_VERSION" ]; then
+   if [ ! -d "$HOME/.sdkman/groovy/$GRAILS_VERSION" ]; then
+      sdk install groovy $GRAILS_VERSION
+   fi
+else
+   echo "******OMAR SETUP ERROR: Environment variable GRAILS_VERSION must be set"
+   exit 1
+fi
+
+if [ ! -z "$GRADLE_VERSION" ]; then
+   if [ ! -d "$HOME/.sdkman/groovy/$GRADLE_VERSION" ]; then
+      sdk install groovy $GRADLE_VERSION
+   fi
+else
+   echo "******OMAR SETUP ERROR: Environment variable GRADLE_VERSION must be set"
+   exit 1
+fi
