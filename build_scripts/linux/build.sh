13cfa310f8be5b6426a01150c16efd461bbbd0b1
Garrett Potts <gpotts@radiantblue.com>
2015-10-21 19:29:13 -0400

    Added statement forsourcing sdk

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -4,14 +4,10 @@ export SCRIPT_DIR=`pwd -P`
 pushd $SCRIPT_DIR/../.. >/dev/null
 export OMAR_DEV_HOME=$PWD
 export OMAR_HOME=$OMAR_DEV_HOME/apps/omar
-#pushd $OMAR_DEV_HOME/.. >/dev/null
-#export OSSIM_DEV_HOME=$PWD
-#export OSSIM_HOME=$OSSIM_DEV_HOME/ossim
-#echo "@@@@@ OMAR_DEV_HOME=$OMAR_DEV_HOME"
-#echo "@@@@@ OSSIM_DEV_HOME=$OSSIM_DEV_HOME"
-#echo "@@@@@ OSSIM_HOME=$OSSIM_HOME"
-#popd >/dev/null
 popd >/dev/null
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+pushd $OMAR_HOME >/dev/null
 
 #
 popd >/dev/null
