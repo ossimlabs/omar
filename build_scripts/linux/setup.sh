2b6b95591b9a849a1b9b48812f045bc0c2553e6d
Garrett Potts <gpotts@radiantblue.com>
2015-10-21 20:10:05 -0400

    Added tests for valid execution of SDK

--- build_scripts/linux/setup.sh
+++ build_scripts/linux/setup.sh
@@ -9,6 +9,12 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 if [ ! -z "$GROOVY_VERSION" ]; then
    if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
       sdk install groovy $GROOVY_VERSION
+      sdkReturnCode=$?
+   if [ $antReturnCode -ne 0 ];then
+       echo "GROOVY_VERSION INSTALL ERROR: sdk install for groovy $GROOVY_VERSION failed.."
+       exit 1;
+   fi
+
    fi
 else
    echo "******OMAR SETUP ERROR: Environment variable GROOVY_VERSION must be set"
@@ -18,6 +24,11 @@ fi
 if [ ! -z "$GRAILS_VERSION" ]; then
    if [ ! -d "$HOME/.sdkman/groovy/$GRAILS_VERSION" ]; then
       sdk install grails $GRAILS_VERSION
+      sdkReturnCode=$?
+      if [ $antReturnCode -ne 0 ];then
+          echo "GRAILS_VERSION INSTALL ERROR: sdk install for grails $GRAILS_VERSION failed.."
+          exit 1;
+      fi
    fi
 else
    echo "******OMAR SETUP ERROR: Environment variable GRAILS_VERSION must be set"
@@ -27,6 +38,10 @@ fi
 if [ ! -z "$GRADLE_VERSION" ]; then
    if [ ! -d "$HOME/.sdkman/groovy/$GRADLE_VERSION" ]; then
       sdk install gradle $GRADLE_VERSION
+      if [ $antReturnCode -ne 0 ];then
+          echo "GRADLE_VERSION INSTALL ERROR: sdk install for groovy $GRADLE_VERSION failed.."
+          exit 1;
+      fi
    fi
 else
    echo "******OMAR SETUP ERROR: Environment variable GRADLE_VERSION must be set"
