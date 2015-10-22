840ae6632f67fb9d889166d984daf7b7f8305bde
Garrett Potts <gpotts@radiantblue.com>
2015-10-21 20:59:35 -0400

    Fixed directory

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -3,7 +3,7 @@ pushd `dirname $0` >/dev/null
 export SCRIPT_DIR=`pwd -P`
 pushd $SCRIPT_DIR/../.. >/dev/null
 export OMAR_DEV_HOME=$PWD
-export OMAR_HOME=$OMAR_DEV_HOME/apps/omar
+export OMAR_HOME=$OMAR_DEV_HOME/apps/omar-app
 popd >/dev/null
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use groovy $GROOVY_VERSION
