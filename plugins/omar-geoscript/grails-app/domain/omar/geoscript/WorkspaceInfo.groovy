00fe8ea528162d97c4f29666060b4fab76a925f4
Scott Bortman <scott.bortman@gmail.com>
2015-12-16 15:39:47 -0500

    A stack overflow was generated everytime one of these objects was printed.   This was cause by a circular reference in the ToString method.  Removed the offending property.

--- plugins/omar-geoscript/grails-app/domain/omar/geoscript/WorkspaceInfo.groovy
+++ plugins/omar-geoscript/grails-app/domain/omar/geoscript/WorkspaceInfo.groovy
@@ -2,7 +2,7 @@ package omar.geoscript
 
 import groovy.transform.ToString
 
-@ToString(includeNames = true)
+@ToString(includeNames = true, excludes = 'layerInfoList')
 class WorkspaceInfo
 {
   String name
