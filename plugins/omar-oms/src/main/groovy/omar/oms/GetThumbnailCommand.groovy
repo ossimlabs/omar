d2ff01b1664979ea09a20f6e90f9b271e6c7e98e
Scott Bortman <scott.bortman@gmail.com>
2015-12-17 13:37:51 -0500

    Must of forgot this on last checkin

--- plugins/omar-oms/src/main/groovy/omar/oms/GetThumbnailCommand.groovy
+++ plugins/omar-oms/src/main/groovy/omar/oms/GetThumbnailCommand.groovy
@@ -1,8 +1,16 @@
 package omar.oms
 
+import grails.validation.Validateable
+import groovy.transform.ToString
+
 /**
  * Created by sbortman on 12/17/15.
  */
-class GetThumbnailCommand
+@ToString( includeNames = true )
+class GetThumbnailCommand implements Validateable
 {
+  String filename
+  Integer entry
+  Integer size
+  String format
 }
