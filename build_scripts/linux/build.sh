a69580d4505c89ede6d149665ad39618dce8ecf6
Garrett Potts <gpotts@radiantblue.com>
2016-01-06 16:15:21 -0500

    Added install shell script and env shell setup script to be source by install and build scripts.

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 pushd `dirname $0` >/dev/null
 export SCRIPT_DIR=`pwd -P`
-pushd $SCRIPT_DIR/../.. >/dev/null
-export OMAR_DEV_HOME=$PWD
-export OMAR_HOME=$OMAR_DEV_HOME/apps/omar-app
 popd >/dev/null
-#source "$HOME/.sdkman/bin/sdkman-init.sh"
-#sdk use groovy $GROOVY_VERSION
-#sdk use grails $GRAILS_VERSION
-#sdk use gradle $GRADLE_VERSION
 
 pushd $OMAR_HOME >/dev/null
-#gradle build
-#grails package
+
 ./gradlew assemble
 
 RETURN_CODE=$?
---
+++ build_scripts/linux/env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+pushd `dirname $0` >/dev/null
+export SCRIPT_DIR=`pwd -P`
+popd >/dev/null
+
+pushd $SCRIPT_DIR/../.. >/dev/null
+export OMAR_DEV_HOME=$PWD
+popd >/dev/null
+
+export OMAR_HOME=$OMAR_DEV_HOME/apps/omar-app
+
+if [ -z "$OSSIM_DEV_HOME" ]; then
+   pushd $OMAR_DEV_HOME/.. > /dev/null
+   export OSSIM_DEV_HOME=$PWD
+   popd >/dev/null
+fi
+if [ -z "$OSSIM_INSTALL_PREFIX" ]; then
+   export OSSIM_INSTALL_PREFIX=$OSSIM_DEV_HOME/install
+fi
+
