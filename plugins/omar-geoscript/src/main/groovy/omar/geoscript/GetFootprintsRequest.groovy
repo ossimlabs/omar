b548c9de5dd797550394125d58db34128955704e
Scott Bortman <scott.bortman@gmail.com>
2016-01-06 12:33:35 -0500

    Not sure why these weren't part of the last commit

--- plugins/omar-geoscript/src/main/groovy/omar/geoscript/GetFootprintsRequest.groovy
+++ plugins/omar-geoscript/src/main/groovy/omar/geoscript/GetFootprintsRequest.groovy
@@ -1,8 +1,24 @@
 package omar.geoscript
 
+import grails.validation.Validateable
+import groovy.transform.ToString
+
 /**
  * Created by sbortman on 1/4/16.
  */
-class GetFootprintsRequest
+@ToString( includeNames = true )
+
+class GetFootprintsRequest implements Validateable
 {
+  String bbox
+  String srs
+
+  Integer width
+  Integer height
+
+  String format
+  Boolean transparent
+  String layers
+  String styles
+  String filter
 }
--- plugins/omar-geoscript/src/main/groovy/omar/geoscript/omar/geoscript/QueryLayer.groovy
+++
@@ -1,8 +0,0 @@
-package omar.geoscript.omar.geoscript
-
-/**
- * Created by sbortman on 1/4/16.
- */
-class QueryLayer
-{
-}
