d7fac12b69156332424409bbaa969c15a87dd690
Garrett Potts <gpotts@radiantblue.com>
2016-01-20 16:31:20 -0500

    Added case insensitive parameter bindings

--- plugins/omar-predio/grails-app/controllers/omar/predio/PredioController.groovy
+++ plugins/omar-predio/grails-app/controllers/omar/predio/PredioController.groovy
@@ -1,5 +1,7 @@
 package omar.predio
 
+import omar.core.BindUtil
+
 class PredioController
 {
    def predioService
@@ -8,12 +10,19 @@ class PredioController
    {
       render ""
    }
-   def rate(RateCommand cmd)
+   def rate()
    {
+      def rateParams = params - params.subMap( ['controller', 'format', 'action'] )
+      def cmd = new RateCommand()
+      BindUtil.fixParamNames( RateCommand, rateParams )
+      bindData( cmd, rateParams )
+
+//      println "*"*40
+//      println cmd
       HashMap result = predioService.rate(cmd)
+
       response.status = result.status.value()
       response.contentType = response.contentType
-
       render result.message
    }
 
