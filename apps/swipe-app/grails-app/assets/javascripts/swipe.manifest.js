7c6872da384468efdf051ab3e59dce52c38a58e8
melbsurfer <melbsurfer@gmail.com>
2016-02-11 13:18:35 -0500

    Refactored swap functionality in Swipe Viewer

--- apps/swipe-app/grails-app/assets/javascripts/swipe.manifest.js
+++ apps/swipe-app/grails-app/assets/javascripts/swipe.manifest.js
@@ -1,11 +1,11 @@
 // Vendor
 //= require webjars/jquery/2.1.4/jquery.min.js
-//= require webjars/openlayers/3.13.0/ol.js
+//= require webjars/openlayers/3.13.0/ol-debug.js
 //= require webjars/bootstrap/3.3.5/js/bootstrap.js
 //= require webjars/angularjs/1.4.8/angular.js
 
 //= <--Swipe-->
 //= require swipe.module.js
-//= require swipe/swipe.controller.js
+//= require swipe/swipe.standalone.controller.js
 
 //= require_self
