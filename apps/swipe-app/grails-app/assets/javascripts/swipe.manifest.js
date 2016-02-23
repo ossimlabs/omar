bf4f17de06f73403eba0793d5a0c67df589e6f91
melbsurfer <melbsurfer@gmail.com>
2016-02-23 08:46:02 -0500

    Swapped ol-debug.js for ol.js and cleaned up some logging

--- apps/swipe-app/grails-app/assets/javascripts/swipe.manifest.js
+++ apps/swipe-app/grails-app/assets/javascripts/swipe.manifest.js
@@ -1,6 +1,6 @@
 // Vendor
 //= require webjars/jquery/2.1.4/jquery.min.js
-//= require webjars/openlayers/3.13.0/ol-debug.js
+//= require webjars/openlayers/3.13.0/ol.js
 //= require webjars/bootstrap/3.3.5/js/bootstrap.js
 //= require webjars/angularjs/1.4.8/angular.js
 //= require webjars/angular-toggle-switch/1.3.0/angular-toggle-switch.js
--- apps/swipe-app/grails-app/controllers/swipe/app/SwipeController.groovy
+++ apps/swipe-app/grails-app/controllers/swipe/app/SwipeController.groovy
@@ -5,7 +5,7 @@ class SwipeController
 {
 	def index()
 	{
-		//def banner = grailsApplication.config.classificationBanner
+
 		def clientParams = grailsApplication.config.swipe.app
 
 		println "clientParams: ${ clientParams }"
