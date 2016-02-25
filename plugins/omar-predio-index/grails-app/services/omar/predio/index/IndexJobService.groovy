7d20fb11b4d838537c4e051008bf258283352910
Garrett Potts <gpotts@radiantblue.com>
2016-02-24 20:53:10 -0500

    Moved the http post to the predio to the service

--- plugins/omar-predio-index/grails-app/services/omar/predio/index/IndexJobService.groovy
+++ plugins/omar-predio-index/grails-app/services/omar/predio/index/IndexJobService.groovy
@@ -1,6 +1,10 @@
 package omar.predio.index
 
 import grails.transaction.Transactional
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.ContentType
+import groovyx.net.http.Method
+import groovyx.net.http.RESTClient
 
 @Transactional
 class IndexJobService {
@@ -14,5 +18,38 @@ class IndexJobService {
         firstObject?.delete()
 
         result
+    }
+    def postItem(def item, def locations, def categories, def eventTime = null, def expireDate=null)
+    {
+        def config = OmarPredioIndexUtils.predioIndexConfig
+        URL url = config.predioUrl
+        String urlPart = "${url.protocol}://${url.authority}"
+        String pathPart = url.path
+
+
+        def http = new HTTPBuilder(urlPart)
+
+        http.request(Method.POST, ContentType.TEXT) {
+            uri.path = pathPart
+            uri.query = [item:item, locations:locations?.join(","), categories:categories?.join(",")]
+            if(eventTime) uri.query.eventTime   = eventTime.toString()
+            if(expireDate) uri.query.expireDate = expireDate.toString()
+            //headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+
+            // response handler for a success response code
+            response.success = { resp, reader ->
+                println "response status: ${resp.statusLine}"
+                println 'Headers: -----------'
+                resp.headers.each { h ->
+                    println " ${h.name} : ${h.value}"
+                }
+
+                ret = reader.getText()
+
+                println 'Response data: -----'
+                println ret
+                println '--------------------'
+            }
+        }
     }
 }
