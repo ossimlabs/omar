1bce9c1d9e8986e39c5905e98db64d998c03eefd
Garrett Potts <gpotts@radiantblue.com>
2016-02-24 20:52:41 -0500

    Working on the indexer.  Moving the submit code to the service

--- plugins/omar-predio-index/grails-app/jobs/omar/predio/index/WfsIndexerJob.groovy
+++ plugins/omar-predio-index/grails-app/jobs/omar/predio/index/WfsIndexerJob.groovy
@@ -2,10 +2,6 @@ package omar.predio.index
 
 import groovy.util.logging.Slf4j
 import groovy.json.JsonSlurper
-import groovyx.net.http.HTTPBuilder
-import groovyx.net.http.ContentType
-import groovyx.net.http.Method
-import groovyx.net.http.RESTClient
 import omar.core.DateUtil
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
@@ -18,34 +14,6 @@ class WfsIndexerJob {
    def concurrent = false
 
    transient indexJobService
-   def setItem(def item, def locations, def categories, def eventTime = null, def expireDate=null)
-   {
-      def http = new HTTPBuilder("http://o2.ossim.org")
-
-      http.request(Method.POST, ContentType.TEXT) {
-         uri.path = "/o2/predio/setItem"
-         uri.query = [item:item, locations:locations?.join(","), categories:categories?.join(",")]
-         if(eventTime) uri.query.eventTime   = eventTime.toString()
-         if(expireDate) uri.query.expireDate = expireDate.toString()
-         //headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-
-         // response handler for a success response code
-         response.success = { resp, reader ->
-            println "response status: ${resp.statusLine}"
-            println 'Headers: -----------'
-            resp.headers.each { h ->
-               println " ${h.name} : ${h.value}"
-            }
-
-            ret = reader.getText()
-
-            println 'Response data: -----'
-            println ret
-            println '--------------------'
-         }
-      }
-
-   }
 
    def execute() {
       log.trace("execute(): Entered ...............")
