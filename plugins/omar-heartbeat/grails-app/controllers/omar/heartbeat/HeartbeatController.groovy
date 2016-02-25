a91093896e45b5061e169b5be7dadc5d2bbb025f
Garrett Potts <gpotts@radiantblue.com>
2016-02-24 21:01:45 -0500

    Modified to just use the info object from grails

--- plugins/omar-heartbeat/grails-app/controllers/omar/heartbeat/HeartbeatController.groovy
+++ plugins/omar-heartbeat/grails-app/controllers/omar/heartbeat/HeartbeatController.groovy
@@ -5,11 +5,13 @@ class HeartbeatController {
   // def grailsApplication
 
     def index() {
-        def result = [
-                status: "alive",
-                name: grailsApplication.config.info.app.name,
-                version: grailsApplication.config.info.app.version
-        ]
+        println grailsApplication.config.info
+        def result = grailsApplication.config.info
+//        def result = [
+//                status: "alive",
+//                name: grailsApplication.config.info.app.name,
+//                version: grailsApplication.config.info.app.version
+//        ]
         render result as JSON
     }
 }
