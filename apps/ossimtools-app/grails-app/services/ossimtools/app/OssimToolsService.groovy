47dfa4b9674c40ade5287db4b1320d0d3a1861dc
Scott Bortman <scott.bortman@gmail.com>
2016-02-26 16:00:19 -0500

    converted byte[] -> image so that it can be streamed

--- apps/ossimtools-app/grails-app/services/ossimtools/app/OssimToolsService.groovy
+++ apps/ossimtools-app/grails-app/services/ossimtools/app/OssimToolsService.groovy
@@ -3,45 +3,76 @@ package ossimtools.app
 import joms.oms.OssimTools
 import geoscript.geom.Point
 import geoscript.proj.Projection
+import omar.oms.ChipperUtil
+
+import java.awt.Point
+import java.awt.image.BufferedImage
+import java.awt.image.DataBuffer
+import java.awt.image.Raster
+
+import javax.imageio.ImageIO
 
 class OssimToolsService
 {
-  static transactional = false
+	static transactional = false
 
-  def grailsApplication
+	def grailsApplication
 
-  def calculateExtent(def lat, def lon, def radius)
-  {
-    def point1 = new Point( lon, lat )
-    def point2 = Projection.transform( point1, 'epsg:4326', 'epsg:3857' )
-    def bounds = /*Projection.transform(*/ point2.buffer( radius )/*, 'epsg:3857', 'epsg:4326' )*/.bounds
+	def calculateExtent( def lat, def lon, def radius )
+	{
+		def point1 = new geoscript.geom.Point( lon, lat )
+		def point2 = Projection.transform( point1, 'epsg:4326', 'epsg:3857' )
+		def bounds = /*Projection.transform(*/ point2.buffer( radius )/*, 'epsg:3857', 'epsg:4326' )*/.bounds
 
 
-    [[bounds.minX, bounds.minY], [bounds.maxX, bounds.maxY]]
-  }
+		[ [ bounds.minX, bounds.minY ], [ bounds.maxX, bounds.maxY ] ]
+	}
 
-  def execTool(def name, def params)
-  {
-     def ossimTool = new OssimTools(name);
-     if (!ossimTool)
-         return;
+	def execTool( def name, def params )
+	{
+		def ossimTool = new OssimTools( name );
+		if ( !ossimTool )
+		{
+			return
+		};
 
-     // Using an LUT implies 3 band output. Allocate buffer to receive product:
-     int numBands = 3
-     byte[] buffer = new byte[hints.width.toInteger() * hints.height.toInteger() * numBands]
+		// Using an LUT implies 3 band output. Allocate buffer to receive product:
+		int numBands = 3
+		//byte[] buffer = new byte[hints.width.toInteger() * hints.height.toInteger() * numBands]
 
-     // Eventually need to let the user select colors. For now use hardcoded LUT on server:
-     params.lut = grailsApplication?.config?.ossimtools?.supportData?.toString() + "hlz.lut"
+		//def numBands = hints.transparent ? 4 : 3
 
-     if (!ossimTool.initialize(params))
-         return;
+		def raster = Raster.createInterleavedRaster(
+				DataBuffer.TYPE_BYTE,
+				hints.width, hints.height,
+				hints.width * numBands, numBands, ( 0..<numBands ) as int[],
+				new Point( 0, 0 ) )
 
-     // Since a new tool is created each time, pass in a bogus map to indicate full AOI should
-     // be computed:
-     def hints = [ foo : "bar" ]
-     if (!ossimTool.getChip(buffer, hints))
-         return;
+		// Eventually need to let the user select colors. For now use hardcoded LUT on server:
+		params.lut = grailsApplication?.config?.ossimtools?.supportData?.toString() + "hlz.lut"
 
-     [contentType: '???', buffer: buffer]
-  }
+		if ( !ossimTool.initialize( params ) )
+		{
+			return
+		};
+
+		// Since a new tool is created each time, pass in a bogus map to indicate full AOI should
+		// be computed:
+		//def hints = [ foo: "bar" ]
+		def hints = [ transparent: "true" ]
+//     if (!ossimTool.getChip(buffer, hints))
+		if ( !ossimTool.getChip( raster.dataBuffer.data, hints ) )
+		{
+			return
+		}
+
+		def colorModel = ChipperUtil.createColorModel(numBands, hints?.transparent)
+		def image = new BufferedImage(colorModel, raster, false, null)
+		def ostream = new ByteArrayOutputStream()
+
+		ImageIO.write(image, 'png', ostream)
+
+//		[ contentType: '???', buffer: buffer ]
+		[ contentType: 'image/png', buffer: buffer ]
+	}
 }
