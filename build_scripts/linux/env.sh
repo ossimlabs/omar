3b1c3471877548773c3ece2d1300d409e2236adb
Garrett Potts <gpotts@radiantblue.com>
2016-04-19 09:22:45 -0400

    Problem with the install prefix

--- build_scripts/linux/env.sh
+++ build_scripts/linux/env.sh
@@ -3,6 +3,6 @@ export OMAR_DEV_HOME=$ROOT_DIR/omar
 export O2_APPS=( "omar-app" "wfs-app" "wms-app" "stager-app" "swipe-app" "superoverlay-app" "jpip-app wmts-app" )
 
 if [ -z "$OMAR_INSTALL_PREFIX" ]; then
-   export OMAR_INSTALL_PREFIX=$OMAR_DEV_HOME/omar-install
+   export OMAR_INSTALL_PREFIX=$ROOT_DIR/omar-install
 fi
 
