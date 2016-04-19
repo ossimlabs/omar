877e378c917e9e65e69284a4fe9f85e19dd5ec49
Garrett Potts <gpotts@radiantblue.com>
2016-04-19 09:27:04 -0400

    Fixed the artifact install to not prefix the directory in the output

--- build_scripts/linux/install.sh
+++ build_scripts/linux/install.sh
@@ -11,8 +11,8 @@ popd >/dev/null
 
 for app in ${O2_APPS[@]} ; do 
    echo "INSTALLING $app"
-   pushd $OMAR_DEV_HOME/apps/$app >/dev/null
-   for artifact in  `ls $PWD/build/libs/*.jar` ; do
+   pushd $OMAR_DEV_HOME/apps/$app/build/libs/ >/dev/null
+   for artifact in  `ls *.jar` ; do
       install -p -m644 -D $artifact ${OMAR_INSTALL_PREFIX}/share/omar/$app/$artifact
       if [ $? -ne 0 ];then
           echo "INSTALL ERROR: $app failed to install..."
