1b39f0ac0b193c201b761b0ca1acc99b6bde8bca
Garrett Potts <gpotts@radiantblue.com>
2016-05-09 09:23:08 -0400

    Added install of the service wrappers to the template location

--- build_scripts/linux/install.sh
+++ build_scripts/linux/install.sh
@@ -20,4 +20,14 @@ for app in ${O2_APPS[@]} ; do
       fi   
    done
    popd >/dev/null
+
+   pushd $OMAR_DEV_HOME/support/linux >/dev/null
+   for artifact in  `ls *-template` ; do
+     install -d -m755 ${OMAR_INSTALL_PREFIX}/share/omar/$app/service-templates
+     install -p -m644 -D $artifact ${OMAR_INSTALL_PREFIX}/share/omar/$app/$artifact
+     sed -e "s/{{program_name}}/${app}/g"  -e "s/{{program_user}}/omar/g" -e "s/{{program_group}}/omar/g" < service-wrapper-systemd-template >${OMAR_INSTALL_PREFIX}/share/omar/$app/service-templates/${app}.service 
+     sed -e "s/{{program_name}}/${app}/g"  -e "s/{{program_user}}/omar/g" -e "s/{{program_group}}/omar/g" < service-wrapper-initd-template >${OMAR_INSTALL_PREFIX}/share/omar/$app/service-templates/${app} 
+     chmod gu+x ${OMAR_INSTALL_PREFIX}/share/omar/$app/service-templates/*
+   done
+   popd > /dev/null
 done
