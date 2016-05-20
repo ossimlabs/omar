d39e9245d99fead6a1640e7e1c5c36e2e0a95e8d
Garrett Potts <gpotts@radiantblue.com>
2016-05-20 09:05:11 -0400

    Adding the builddocs script to build a tar up the docuementation

---
+++ build_scripts/linux/builddocs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pushd `dirname $0` >/dev/null
+export SCRIPT_DIR=`pwd -P`
+popd >/dev/null
+
+pushd $SCRIPT_DIR/../../.. >/dev/null
+export ROOT_DIR=$PWD
+popd >/dev/null
+
+. $SCRIPT_DIR/env.sh
+
+pushd $OMAR_DEV_HOME
+mkdocs build -d omardocs
+if [ $? -ne 0 ];then
+    echo "BUILD DOCS ERROR: failed to build docs using mkdocs..."
+    exit 1
+fi 
+
+tar cvfz omardocs.tgz omardocs
+if [ $? -ne 0 ];then
+    echo "BUILD DOCS ERROR: failed to zip up the docs..."
+    exit 1
+fi 
