105fbae6dbeb4b0e4a3070a2fcba087691fc6bb0
Garrett Potts <gpotts@radiantblue.com>
2016-06-07 07:54:28 -0400

    Added sqs-app to the build process

--- build_scripts/linux/env.sh
+++ build_scripts/linux/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export OMAR_DEV_HOME=$ROOT_DIR/omar
-export O2_APPS=( "omar-app" "wfs-app" "wms-app" "stager-app" "swipe-app" "superoverlay-app" "jpip-app wmts-app" )
+export O2_APPS=( "omar-app" "sqs-app" "wfs-app" "wms-app" "stager-app" "swipe-app" "superoverlay-app" "jpip-app wmts-app" )
 
 if [ -z "$OMAR_INSTALL_PREFIX" ]; then
    export OMAR_INSTALL_PREFIX=$ROOT_DIR/install
