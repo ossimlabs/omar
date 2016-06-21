15d30c625b22728a29bba46869d73818c807b093
Garrett Potts <gpotts@radiantblue.com>
2016-06-21 14:50:14 -0400

    Added support for staging file in the background

---
+++ plugins/omar-stager/grails-app/jobs/omar/stager/StageFileJob.groovy
@@ -0,0 +1,42 @@
+package omar.stager
+import omar.core.ProcessStatus
+
+class StageFileJob {
+   def stagerService
+
+   static triggers = {
+      simple name: 'StageFileJobTrigger', group: 'StageFileJobGroup', repeatInterval: 5000l
+   }
+
+   def execute() {
+      log.trace "Entered........."
+      def fileRecord
+      while(fileRecord = stagerService.nextFileToStage())
+      {
+         try
+         {
+            def result = stagerService.stageFileJni([filename:fileRecord.filename,
+                                        buildOverviews: fileRecord.buildOverviews,
+                                        buildHistograms:fileRecord.buildHistograms,
+                                        overviewCompressionType: fileRecord.overviewCompressionType,
+                                        overviewType: fileRecord.overviewType
+            ])
+            if(result.status>=300)
+            {
+               log.error  result?.message?:"Failed to stage file ${fileRecord.filename}"
+               stagerService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, result?.message?:"Failed to stage file ${fileRecord.filename}")
+            }
+            else
+            {
+               stagerService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File ${fileRecord.filename} successfully staged")
+            }
+         }
+         catch(e)
+         {
+            log.error e.toString()
+            stagerService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, "File ${fileRecord.filename} NOT staged with error ${e}")
+         }
+      }
+      log.trace "Leaving..........."
+   }
+}
