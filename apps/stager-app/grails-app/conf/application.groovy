d46e2827b4036102be5c3af36164de56c70d08d1
Scott Bortman <scott.bortman@gmail.com>
2016-07-18 13:45:58 -0400

    Added application.groovy files for ALL apps that were missing

--- apps/stager-app/grails-app/conf/application.groovy
+++ apps/stager-app/grails-app/conf/application.groovy
@@ -1,4 +1,8 @@
 grails.gorm.default.mapping = {
-  cache true
-  id generator: 'identity'
+	cache true
+	id generator: 'identity'
 }
+
+//grails.serverURL = "http://${Inet4Address.localHost.hostAddress}/o2"
+//grails.serverURL = "http://localhost/omar-app"
+//grails.assets.url = "http://localhost/omar-app/assets/"
