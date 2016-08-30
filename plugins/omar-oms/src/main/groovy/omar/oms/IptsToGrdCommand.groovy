6824d99280113150e765dfdb9363b2d03c5a54d2
Garrett Potts <gpotts@radiantblue.com>
2016-08-30 14:48:18 -0400

    Added support to make the arguments to be similar in the imageDistance method

--- plugins/omar-oms/src/main/groovy/omar/oms/IptsToGrdCommand.groovy
+++ plugins/omar-oms/src/main/groovy/omar/oms/IptsToGrdCommand.groovy
@@ -11,7 +11,7 @@ class IptsToGrdCommand implements Validateable
 {
    String filename
    Integer entryId = 0
-   def imagePoints
+   def pointList
    Boolean pqeIncludePositionError = false
    String pqeEllipsePointType = "none" // can be "none" or "array", "polygon", "linestring"
    Double pqeProbabilityLevel = 0.9
@@ -20,7 +20,7 @@ class IptsToGrdCommand implements Validateable
    static constraints = {
       filename blank:false, nullable:false
       entryId nullable:true
-      imagePoints nullable:false
+      pointList nullable:false
       pqeIncludePositionError nullable:true, validator: {value, object -> }
       pqeEllipsePointType nullable:true , validator: {value, object -> }
       pqeProbabilityLevel nullable:true , validator: {value, object -> }
