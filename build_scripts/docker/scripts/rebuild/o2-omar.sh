c02b2563879d0dea68c438ce1c77e46d6f216582
melbsurfer <melbsurfer@gmail.com>
2016-09-01 15:54:41 -0400

    Modified the compose environment, and broke it into a local and AWS versions.

--- build_scripts/docker/scripts/rebuild/o2-omar.sh
+++ build_scripts/docker/scripts/rebuild/o2-omar.sh
@@ -6,4 +6,4 @@ docker rm o2-omar
 echo "Removing o2-omar image..."
 docker rmi ossimlabs/o2-omar
 echo "Running o2-omar with docker-compose..."
-docker-compose --file ../../docker-compose.yml run -d --service-ports --name o2-omar o2-omar
+docker-compose --file ../../docker-compose-local.yml run -d --service-ports --name o2-omar o2-omar
--- build_scripts/docker/scripts/rebuild/o2-superoverlay.sh
+++ build_scripts/docker/scripts/rebuild/o2-superoverlay.sh
@@ -6,4 +6,4 @@ docker rm o2-superoverlay
 echo "Removing o2-superoverlay image..."
 docker rmi ossimlabs/o2-superoverlay
 echo "Running o2-superoverlay with docker-compose..."
-docker-compose --file ../../docker-compose.yml run -d --service-ports --name o2-superoverlay o2-superoverlay
+docker-compose --file ../../docker-compose-local.yml run -d --service-ports --name o2-superoverlay o2-superoverlay
