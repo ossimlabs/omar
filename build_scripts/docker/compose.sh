67aef1c9145882005b0f7703d07daf2ec6c7543b
melbsurfer <melbsurfer@gmail.com>
2016-09-06 18:44:44 -0400

    Modified compose script to work with local and AWS env., and also up/down.

--- build_scripts/docker/compose.sh
+++ build_scripts/docker/compose.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo "Running docker-compose $1 using local settings..."
-docker-compose --file=docker-compose-local.yml $1 #-d
-
-
+echo "Running docker-compose $2 using $1 settings..."
+docker-compose --file=docker-compose-$1.yml $2 #-d
 
 # echo "Running wait for it..."
 # ./wait-for-it.sh --wget http://192.168.99.100/o2-wms/api --timeout=660 --strict -- ./docker-compose-local-wms.sh
