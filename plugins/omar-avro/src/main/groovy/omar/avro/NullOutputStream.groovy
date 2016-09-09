70f56679a2c4cbe4d956b1b448f474188688fae7
Garrett Potts <gpotts@radiantblue.com>
2016-09-09 04:59:56 -0400

    Added a null output consumer to simulate a quiet output and to simulate /dev/null redirect

---
+++ plugins/omar-avro/src/main/groovy/omar/avro/NullOutputStream.groovy
@@ -0,0 +1,10 @@
+package omar.avro
+
+/**
+ * Created by gpotts on 9/9/16.
+ */
+class NullOutputStream extends OutputStream {
+   @Override
+   public void write(int b) throws IOException {
+   }
+}
