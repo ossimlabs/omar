da5730aa24261d6d9985be3623a0c9c05e58fc35
Scott Bortman <scott.bortman@gmail.com>
2016-09-13 15:58:51 -0400

    Modded to allow multiple docker-compose-<name>.yml files.

--- build_scripts/docker/compose.sh
+++ build_scripts/docker/compose.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-if [ "$1" == "local" ] || [ "$1" == "oc2s" ]; then
+string=$1
+prefix="docker-compose-"
+suffix=".yml"
+
+file=$prefix$1$suffix
+
+display_usage() {
+  echo "Usage: ./compose.sh <env> <up|down> [sudo]"
+  echo "Where env in:"
+  for env in `ls $prefix*$suffix`; do
+    string=$env
+    env=${string#$prefix}
+    env=${env%$suffix}
+    echo "   ${env}"
+  done
+}
+
+if [ -a $file ]; then
 
   if [ "$2" == "up" ] || [ "$2" == "down" ]; then
 
-    if [ "$1" == "local" ]; then
-
-    echo "Running docker-compose $2 using $1 settings..."
-    docker-compose --file=docker-compose-$1.yml $2 #-d
-
-    elif [ "$1" == "oc2s" ]; then
-
-    echo "Running docker-compose $2 using $1 settings..."
-    sudo docker-compose --file=docker-compose-$1.yml $2 #-d
-
-    fi
+    # if [ "$1" == "local" ]; then
+    #
+    # echo "Running docker-compose $2 using $1 settings..."
+    # docker-compose --file=docker-compose-$1.yml $2 #-d
+    #
+    # elif [ "$1" == "oc2s" ]; then
+    #
+    # echo "Running docker-compose $2 using $1 settings..."
+    $3 docker-compose --file=docker-compose-$1.yml $2 #-d
+    #
+    # fi
 
   else
-
-    echo "Usage: ./compose.sh < local | oc2s > < up | down >"
-
+    display_usage
+    exit 1
   fi
 
 else
-
-  echo "Usage: ./compose.sh < local | oc2s > < up | down >"
-
+  display_usage
+  exit 1
 fi
