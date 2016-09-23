3eb42ccf2c9b15053295c99e06f04b8c2ecb046b
Scott Bortman <scott.bortman@gmail.com>
2016-09-23 10:15:35 -0400

    o2-wcs as been assimilated into spring-cloud

--- apps/wcs-app/grails-app/init/wcs/app/Application.groovy
+++ apps/wcs-app/grails-app/init/wcs/app/Application.groovy
@@ -2,7 +2,9 @@ package wcs.app
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
