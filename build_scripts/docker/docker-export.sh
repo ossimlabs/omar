567e412bbcf86eb8527d19631e81bdb92da33370
oscarkramer <okramer@radiantblue.com>
2016-09-28 17:30:41 -0400

    Centralized some common functions

--- build_scripts/docker/docker-export.sh
+++ build_scripts/docker/docker-export.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-function runCommand() 
-{
-  echo $*
-  eval $*
-  if [ $? != 0 ] ; then 
-    echo "ERROR: Failed while executing command: <$*>."
-    echo; exit 1;
-  fi
-}
+#=================================================================================
+#
+# This script serializes the O2 docker container images to a TAR archive and
+# thewn uploads them to an S3 bucket. The images must be available on the local
+# machine's docker instance. The destination S3 bucket is specified in the 
+# $S3_DELIVERY_BUCKET environment variable. Upon successful upload, the local
+# copies of the TAR files are deleted from the local machine.
+#
+#=================================================================================
 
-if [ -z $S3_DELIVERY_BUCKET ]; then
-  S3_DELIVERY_BUCKET="s3://o2-delivery/temp"
-  echo "WARNING: No URL specified for S3 delivery bucket. Defaulting S3_DELIVERY_BUCKET = <$S3_DELIVERY_BUCKET>"
-  echo;
-fi
-
-# Assigns O2_APPS and TAG:
+# Assigns O2_APPS and TAG and functions:
 . docker-common.sh
 
 mkdir -p image_export 
@@ -24,26 +18,26 @@ rm -rf image_export/*.tar
 pushd image_export
 
 for app in ${O2_APPS[@]} ; do
-   imagename="ossimlabs/${app}:${TAG}"
+   getImageName ${app} ${TAG}
    exists=$( docker images | grep -c -e "$app[ ]\{2,\}${TAG}") 
    
    if [ $exists != "0" ]; then
       
       # Export the image to local tar file
-      tarfile="${app}-${TAG}.tgz"
-      echo "Exporting docker image ${imagename} to $tarfile"
-      runCommand docker save ${imagename} \| gzip \> $tarfile
+      getTarFileName ${app} ${TAG}
+      echo "Exporting docker image $imagename to $tarfilename"
+      runCommand docker save $imagename \| gzip \> $tarfilename
    
       # upload the tar file to S3
       echo "Uploading $tarfile to $S3_DELIVERY_BUCKET"
       runCommand aws s3 cp $tarfile $S3_DELIVERY_BUCKET/
-      echo "SUCCESS: Image <${imagename}> successfully exported and archived. "
+      echo "SUCCESS: Image <$imagename> successfully exported and archived. "
       
       # Whack the local tar file:
-      runCommand rm $tarfile
+      runCommand rm $tarfilename
       
    else
-      echo "Skipping export of missing image ${imagename}."
+      echo "Skipping export of missing image $imagename."
    fi
 done
 popd
--- build_scripts/docker/docker-rmi-all.sh
+++ build_scripts/docker/docker-rmi-all.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 . docker-common.sh
-for app in ${O2_APPS[@]} ; do
-   image=ossimlabs/${app}:${TAG}
-   echo "Removing docker image ${image} "
-   docker rmi -f ${image}
+
+containers=($(docker ps -q))
+for container in ${containers[@]} ; do
+   echo "Stopping docker container ${container} "
+   docker stop ${container}
 done
+
+for app in ${O2_APPS[@]} ; do
+   getImageName $app $TAG
+   exists=$( docker images | grep -c -e "$app[ ]\{2,\}${TAG}") 
+   if [ $exists != "0" ]; then
+     echo "Removing docker image ${imagename} "
+     docker rmi -f ${imagename}
+   else
+     echo "Skipping missing ${imagename}."
+   fi
+done
+
+echo; echo Remaining images:
+docker images
+echo
