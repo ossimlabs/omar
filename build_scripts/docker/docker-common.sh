13dbd62f7778f6349a91ce3095a80a54ea85d800
Garrett Potts <gpotts@radiantblue.com>
2016-09-29 13:02:14 -0400

    Misplaced] for ig

--- build_scripts/docker/docker-common.sh
+++ build_scripts/docker/docker-common.sh
@@ -21,7 +21,7 @@ O2_APPS=("o2-base")
 O2_APPS+=($(ls -d o2-* | sed -e "s/o2-base//g"))
 O2_APPS+=("tlv")
 
-if [ -z $DOCKER_URI] ; then
+if [ -z $DOCKER_URI ] ; then
   export DOCKER_URI="ossimlabs"
 fi
 
