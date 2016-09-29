0bddd643d9d1b05419851a3c295da52f86b92129
Garrett Potts <gpotts@radiantblue.com>
2016-09-29 12:44:09 -0400

    Adding docker aws build scripts

--- build_scripts/docker/dockeraws-build.sh
+++ build_scripts/docker/dockeraws-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #=================================================================================
 #
 # Performs a build of all O2 docker images. Previous O2 images are removed from 
@@ -8,7 +7,29 @@
 #=================================================================================
 
 # Assigns O2_APPS, TAG and functions:
+export DOCKER_URI="320588532383.dkr.ecr.us-east-1.amazonaws.com"
+
 . docker-common.sh
+
+
+function createRepositories()
+{
+    repositories=$1[@]
+    currentRepositories=`aws ecr describe-repositories --region us-east-1`
+    a=("${!repositories}")
+    for i in "${a[@]}" ; do
+       repoCheck=`echo $currentRepositories | grep $i`
+       if [ "$repoCheck" = "" ] ; then
+         aws ecr create-repository --region us-east-1 --repository-name $i
+       fi
+    done
+
+}
+
+createRepositories O2_APPS
+
+# Create login credentials for docker
+eval `aws ecr get-login --region us-east-1`
 
 for app in ${O2_APPS[@]} ; do
    echo "Building ${app} docker image"
