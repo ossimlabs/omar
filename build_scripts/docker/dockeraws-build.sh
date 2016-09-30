825a4d40e83e6813484622f7a227e95394dd1b54
Garrett Potts <gpotts@radiantblue.com>
2016-09-30 06:56:33 -0400

    Moved for now the ENV variable DOCKER_REGISTRY_URI that defines our docker registry to the build pipeline ENV location

--- build_scripts/docker/dockeraws-build.sh
+++ build_scripts/docker/dockeraws-build.sh
@@ -7,7 +7,6 @@
 #=================================================================================
 
 # Assigns O2_APPS, TAG and functions:
-export DOCKER_REGISTRY_URI="320588532383.dkr.ecr.us-east-1.amazonaws.com"
 
 . docker-common.sh
 
