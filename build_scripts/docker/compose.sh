f1e03a17ab11ac4074df2310b93de85543c5b87f
oscarkramer <okramer@radiantblue.com>
2016-10-04 16:45:34 -0400

    Modified all dockerfiles to reference new registry

--- build_scripts/docker/compose.sh
+++ build_scripts/docker/compose.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 
-string=$1
-prefix="docker-compose-"
-suffix=".yml"
-
-file=$prefix$1$suffix
+# Builds and runs docker stack for O2.
 
 display_usage() {
-  echo "Usage: ./compose.sh <env> <up|down> [sudo]"
-  echo "Where env in:"
-  for env in `ls $prefix*$suffix`; do
-    string=$env
-    env=${string#$prefix}
-    env=${env%$suffix}
-    echo "   ${env}"
-  done
+  echo "Usage: ./compose.sh <local|o2|oc2s> <up|down> [sudo]"
 }
 
-if [ -a $file ]; then
-
-  if [ "$2" == "up" ] || [ "$2" == "down" ]; then
-
-    # if [ "$1" == "local" ]; then
-    #
-    # echo "Running docker-compose $2 using $1 settings..."
-    # docker-compose --file=docker-compose-$1.yml $2 #-d
-    #
-    # elif [ "$1" == "oc2s" ]; then
-    #
-    # echo "Running docker-compose $2 using $1 settings..."
-    $3 docker-compose --file=docker-compose-$1.yml $2 #-d
-    #
-    # fi
-    if [ $? -ne 0 ]; then
-      echo "Ignoring errors..."
-    fi
-  else
-    display_usage
-    exit 1
-  fi
-
+if [ "$1" == "local" ]; then
+  DOCKER_REGISTRY_URL="320588532383.dkr.ecr.us-east-1.amazonaws.com"
+  DOCKER_HOST_URL="o2.radiantbluecloud.com"
+  AWS_CREDENTIALS_PATH="/home/jenkins/.aws:/root/.aws"
+elif [ "$1" == "o2" ]; then
+  DOCKER_REGISTRY_URL="320588532383.dkr.ecr.us-east-1.amazonaws.com"
+  DOCKER_HOST_URL="o2.radiantbluecloud.com"
+  AWS_CREDENTIALS_PATH="/home/jenkins/.aws:/root/.aws"
+elif [ "$1" == "o2" ]; then
+  DOCKER_REGISTRY_URL="320588532383.dkr.ecr.us-east-1.amazonaws.com"
+  DOCKER_HOST_URL="o2.radiantbluecloud.com"
+  AWS_CREDENTIALS_PATH="/home/jenkins/.aws:/root/.aws"
 else
   display_usage
   exit 1
 fi
+
+if [ "$2" == "up" ] || [ "$2" == "down" ]; then
+
+ $3 docker-compose  $2 
+ if [ $? -ne 0 ]; then
+   echo "Ignoring errors..."
+ fi
+else
+ display_usage
+ exit 1
+fi
+
+
