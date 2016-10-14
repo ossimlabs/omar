aa64a4ba6f5f23302ac8f4cc2cbcc286d85b3243
melbsurfer <melbsurfer@gmail.com>
2016-10-14 11:50:19 -0400

    Turned off external file logging in all-app.

--- apps/all-app/grails-app/conf/logback.groovy
+++ apps/all-app/grails-app/conf/logback.groovy
@@ -8,12 +8,12 @@ appender('STDOUT', ConsoleAppender) {
     }
 }
 
-appender('FILE', FileAppender) {
-    file = "log.txt"
-    encoder(PatternLayoutEncoder) {
-      pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
-    }
-}
+// appender('FILE', FileAppender) {
+//     file = "log.txt"
+//     encoder(PatternLayoutEncoder) {
+//       pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+//     }
+// }
 
 root(ERROR, ['STDOUT', 'FILE'])
 
