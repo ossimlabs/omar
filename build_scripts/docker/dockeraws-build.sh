cfa16aea5dd8e44fe8efdb9da1b0bbf9501dd4b5
Garrett Potts <gpotts@radiantblue.com>
2016-12-09 12:09:54 -0500

    Fixed the build for docker.  Forgot to take out an argument

--- build_scripts/docker/dockeraws-build.sh
+++ build_scripts/docker/dockeraws-build.sh
@@ -90,7 +90,7 @@ for app in ${O2_APPS[@]} ; do
      if [ "${app}" == "o2-base" ] ; then
         docker build  --build-arg OSSIM_GIT_BRANCH=${OSSIM_GIT_BRANCH} --no-cache -t ${imagename} .
      else
-        docker build  --build-arg --no-cache -t ${imagename} .
+        docker build  --no-cache -t ${imagename} .
      fi
 
      if [ $? -ne 0 ]; then
