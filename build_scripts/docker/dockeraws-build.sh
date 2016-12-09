eb343f39ed253c709a469398eb6deb2bf7075caa
Garrett Potts <gpotts@radiantblue.com>
2016-12-09 08:07:05 -0500

    Only pass the argument to the image that uses it

--- build_scripts/docker/dockeraws-build.sh
+++ build_scripts/docker/dockeraws-build.sh
@@ -86,8 +86,12 @@ for app in ${O2_APPS[@]} ; do
      getImageName ${app} ${TAG}
      cp Dockerfile Dockerfile.back
      sed -i -e "s/FROM.*ossimlabs.*o2-base/FROM ${DOCKER_REGISTRY_URI}\/o2-base\:${TAG}/" Dockerfile
-     docker build  --build-arg OSSIM_GIT_BRANCH=${OSSIM_GIT_BRANCH} --no-cache -t ${imagename} .
-          
+     
+     if [ "${app}" != "o2-base" ] ; then
+        docker build  --build-arg OSSIM_GIT_BRANCH=${OSSIM_GIT_BRANCH} --no-cache -t ${imagename} .
+     else
+        docker build  --build-arg --no-cache -t ${imagename} .
+     fi
      if [ $? -ne 0 ]; then
        echo; echo "ERROR: Building container ${app} with tag ${TAG}"
        popd
