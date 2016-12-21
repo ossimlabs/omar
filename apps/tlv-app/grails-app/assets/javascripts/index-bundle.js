97d84bd97622e19c8fa402f77621ecab1abaf1a2
knaquin <knaquin@radiantblue.com>
2016-12-21 09:51:35 -0500

    Added the momentjs webjar.

--- apps/tlv-app/grails-app/assets/javascripts/index-bundle.js
+++ apps/tlv-app/grails-app/assets/javascripts/index-bundle.js
@@ -1,6 +1,6 @@
 //= require webjars/jquery/2.2.0/jquery.js
 //= require webjars/bootstrap/3.3.5/js/bootstrap.js
-//= require moment-2.10.6.js
+//= require webjars/momentjs/2.16.0/moment.js
 //= require bootstrap-datetimepicker-4.15.35
 //= require bootstrap-slider
 //= require webjars/cesium/1.21.0/Build/Cesium/Cesium.js
