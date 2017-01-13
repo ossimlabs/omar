c6af6185b0c78cb99720b828105d0d346a5e1d8a
Scott Bortman <scott.bortman@gmail.com>
2017-01-13 13:36:51 -0500

    This files seem to be missing from git

---
+++ apps/omar-app/grails-app/controllers/omar/app/UrlMappings.groovy
@@ -0,0 +1,23 @@
+package omar.app
+
+class UrlMappings {
+
+    static mappings = {
+	"/views/**"(controller: 'views', action: 'renderView')
+
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        // "/" {
+        //     action = 'index'
+        //     controller = 'omar'
+        // }
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+
+}
