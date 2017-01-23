c42c8f1bbb5ace4766c7696e5bb342b6f2767ada
knaquin <knaquin@radiantblue.com>
2017-01-23 14:19:58 -0500

    Adding an extent restriction to each raster layer.

--- plugins/tlv-network-specific/grails-app/services/network_specific/o2/SearchO2Service.groovy
+++ plugins/tlv-network-specific/grails-app/services/network_specific/o2/SearchO2Service.groovy
@@ -13,57 +13,9 @@ class SearchO2Service {
 	def mathConversionService
 
 
-	def extractMetadata(xml) {
-		def metadata = [
-			accessDate: xml.access_date?.text() ?: null,
-			acquisitionDate: xml.acquisition_date?.text() ?: null,
-			azimuthAngle: xml.azimuth_angle?.text().isNumber() ? xml.azimuth_angle.text() as Double : null,
-			beNumber: xml.be_number?.text() ?: null,
-			bitDepth: xml.bit_depth?.text() ?: null,
-			className: xml.class_name?.text() ?: null,
-			cloudCover: xml.cloud_cover?.text().isNumber() ? xml.cloud_cover.text() as Double : null,
-			countryCode: xml.country_code?.text() ?: null,
-			dataType: xml.data_type?.text() ?: null,
-			description: xml.description?.text() ?: null,
-			entryId: xml.entry_id?.text().isNumber() ? xml.entry_id.text() as Integer : null,
-			excludePolicy: xml.exclude_policy?.text() ?: null,
-			fileType: xml.file_type?.text() ?: null,
-			filename: xml.filename?.text() ?: null,
-			grazingAngle: xml.grazing_angle?.text().isNumber() ? xml.grazing_angle.text() as Double : null,
-			gsdUnit: xml.gsd_unit?.text() ?: null,
-			gsdX: xml.gsdx?.text().isNumber() ? xml.gsdx.text() as Double : null,
-			gsdY: xml.gsdy?.text().isNumber() ? xml.gsdy.text() as Double : null,
-			height: xml.height?.text().isNumber() ? xml.height.text() as Integer : null,
-			id: xml.id?.text() ?: null,
-			imageCategory: xml.image_category?.text() ?: null,
-			imageId: xml.image_id?.text() ?: null,
-			imageRepresentation: xml.image_representation?.text() ?: null,
-			indexId: xml.index_id?.text() ?: null,
-			ingestDate: xml.ingest_date?.text() ?: null,
-			isorce: xml.isorce?.text() ?: null,
-			leepForever: xml.keep_forever?.text() ?: null,
-			missionId: xml.mission_id?.text() ?: null,
-			niirs: xml.niirs?.text().isNumber() ? xml.niirs.text() as Double : null,
-			numberOfBands: xml.number_of_bands?.text().isNumber() ? xml.number_of_bands.text() as Integer : null,
-			numberOfResLevels: xml.number_of_res_levels?.text().isNumber() ? xml.number_of_res_levels.text() as Integer : null,
-			organization: xml.organization?.text() ?: null,
-			productId: xml.productId?.text() ?: null,
-			receiveDate: xml.receive_date?.text() ?: null,
-			releaseId: xml.release_id?.text().isNumber() ? xml.release_id.text() as Double : null,
-			securityClassification: xml.security_classification?.text() ?: null,
-			securityCode: xml.security_code?.text() ?: null,
-			sensorId: xml.sensor_id?.text() ?: null,
-			sunAzimuth: xml.sun_azimuth?.text() ? xml.sun_azimuth.text() as Double : null,
-			sunElevation: xml.sun_elevation?.text().isNumber() ? xml.sun_elevation.text() as Double : null,
-			targetId: xml.target_id?.text() ?: null,
-			title: xml.title?.text() ?: null,
-			wacCode: xml.wac_code?.text() ?: null,
-			width: xml.width?.text().isNumber() ? xml.width.text() as Integer : null,
-			version: xml.version?.text() ?: null
-		]
-
-		def footprint = xml.ground_geom?.MultiPolygon?.polygonMember?.Polygon?.outerBoundaryIs?.LinearRing?.coordinates?.text()
-		metadata.put("footprint", footprint ? footprint?.split(" ").collect({ it.split(",").collect({ it as Double}) }) : null)
+	def extractMetadata( json ) {
+		def metadata = json.properties
+		metadata.put( "footprint", json.geometry ?: null )
 
 
 		return metadata
@@ -73,15 +25,13 @@ class SearchO2Service {
 		def images = []
 		features.each() {
 			def metadata = extractMetadata(it)
-
-			def image = [:]
-			image.acquisitionDate = metadata.acquisitionDate
-			image.indexId = metadata.indexId
-			image.imageId = metadata.imageId ?: (metadata.title ?: new File(metadata.filename).name)
-			image.library = library.name
-			image.metadata = metadata
-			image.type = library.layerType
-			images.push(image)
+			images.push([
+				acquisitionDate: metadata.acquisition_date,
+				imageId: metadata.image_id ?: ( metadata.title ?: new File( metadata.filename ).name ),
+				library: library.name,
+				metadata: metadata,
+				type: library.layerType
+			])
 		}
 
 
@@ -139,15 +89,16 @@ class SearchO2Service {
 		queryUrl += "?filter=" + URLEncoder.encode(filter)
 
 		queryUrl += "&maxResults=${params.maxResults}"
+		queryUrl += "&outputFormat=JSON"
 		queryUrl += "&request=getFeature"
 		queryUrl += "&service=WFS"
 		queryUrl += "&typeName=omar:raster_entry"
 		queryUrl += "&version=1.1.0"
 println queryUrl
 
-		def xml = httpDownloadService.serviceMethod([url: queryUrl])
+		def json = httpDownloadService.serviceMethod( [url: queryUrl] )
 
-		def images = xml?.featureMembers ? processResults(xml.featureMembers.raster_entry) : []
+		def images = json?.features ? processResults( json.features ) : []
 
 
 		return images
