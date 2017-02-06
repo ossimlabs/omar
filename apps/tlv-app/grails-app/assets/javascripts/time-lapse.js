dfab1dc8994162fe533842d739677cb31f3a6b88
knaquin <knaquin@radiantblue.com>
2017-02-06 13:05:45 -0500

    Adding in image selection code in the summary table.

--- apps/tlv-app/grails-app/assets/javascripts/time-lapse.js
+++ apps/tlv-app/grails-app/assets/javascripts/time-lapse.js
@@ -1,64 +1,6 @@
 function aFeatureHasBeenSelected(feature, event) {}
 
-function buildSummaryTable() {
-	var table = $( "#timeLapseSummaryTable" )[0];
-
-	for ( var i = table.rows.length - 1; i >= 0; i-- ) { table.deleteRow( i ); }
-
-	var row = table.insertRow( 0 );
-	var cell = row.insertCell( row.cells.length );
-	row.insertCell( row.cells.length );
-	var keys = [ "imageId", "acquisitionDate" ];
-	$.each(
-		keys,
-		function( i, x ) {
-			var cell = row.insertCell( row.cells.length );
-			$( cell ).append( x.capitalize().replace( /([A-Z])/g, " $1" ) );
-		}
-	);
-
-	$.each(
-		tlv.layers,
-		function( i, x ) {
-			row = table.insertRow( table.rows.length );
-			cell = row.insertCell( row.cells.length );
-			$( cell ).append( i + 1 );
-
-			cell = row.insertCell( row.cells.length );
-			$( cell ).css( "white-space", "nowrap" );
-			var downButton = "<span class = 'glyphicon glyphicon-arrow-down' title = 'Move Down'></span>";
-			var upButton = "<span class = 'glyphicon glyphicon-arrow-up' title = 'Move Up'></span>";
-
-			if ( i == 0 ) {
-				$( cell ).append( "<a href = javascript:moveLayerDownInStack(" + i + ");buildSummaryTable();>" + downButton + "</a>" );
-			}
-			else if ( i == tlv.layers.length - 1 ) { $( cell ).append( "<a href = javascript:moveLayerUpInStack(" + i + ");buildSummaryTable();>" + upButton + "</a>" ); }
-			else {
-				$( cell ).append( "<a href = javascript:moveLayerUpInStack(" + i + ");buildSummaryTable();>" + upButton + "</a>" );
-				$( cell ).append( "<a href = javascript:moveLayerDownInStack(" + i + ");buildSummaryTable();>" + downButton + "</a>" );
-			}
-
-			cell = row.insertCell( row.cells.length );
-			$( cell ).append( x.imageId );
-
-			cell = row.insertCell( row.cells.length );
-			$( cell ).append( x.acquisitionDate + "z" );
-
-			cell = row.insertCell( row.cells.length );
-			var span = document.createElement( "span" );
-			span.className = "glyphicon glyphicon-trash";
-
-			var deleteButton = document.createElement( "button" );
- 			deleteButton.className = "btn btn-primary btn-xs";
- 			deleteButton.onclick = function() {
-				deleteFrame( i );
-				buildSummaryTable();
-			};
-			deleteButton.appendChild( span );
-			$( cell ).append( deleteButton );
-		}
-	);
-}
+function buildSummaryTable() {}
 
 function calculateInitialViewBbox() {
         var bbox;
