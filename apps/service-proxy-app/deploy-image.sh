fb82f01e39b442035fc3b79b1d52553ae89021ef
Scott Bortman <scott.bortman@gmail.com>
2017-02-17 10:12:26 -0500

    Added deploy script

---
+++ apps/service-proxy-app/deploy-image.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+APP_NAME=service-proxy
+APP_TAG=latest
+DOCKER_REGISTRY_URL=docker-registry-default.o2.radiantbluecloud.com
+REGISTRY_PROJECT_NAME=o2
+OPENSHIFT_URL=https://openshift-master.radiantbluecloud.com:8443
+
+./gradlew assemble
+docker build -t $APP_NAME:$APP_TAG .
+docker tag $APP_NAME:latest $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
+oc login $OPENSHIFT_URL
+#oc project omar-dev
+docker login -p `oc whoami -t` -e unused -u unused $DOCKER_REGISTRY_URL
+
+CMD="docker push  $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG"
+$CMD
