c258720fee3627ebfdb49690e03d9caf80c65837
Scott Bortman <scott.bortman@gmail.com>
2017-02-17 12:45:24 -0500

    Parameterized modapps.sh Added sed stuff to replace FROM line in Dockerfile Added user stuff to service-proxy Dockerfile

--- apps/geoscript-app/modapps.sh
+++ apps/geoscript-app/modapps.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+APP_NAME=geoscript-app
+APP_TAG=latest
+DOCKER_REGISTRY_URL=docker-registry-default.o2.radiantbluecloud.com
+REGISTRY_PROJECT_NAME=o2
+OPENSHIFT_URL=https://openshift-master.radiantbluecloud.com:8443
+
 ./gradlew assemble
-docker build -t geoscript-app:latest .
-#docker run --rm -p 8080:8080 geoscript-app
-docker tag geoscript-app:latest docker-registry-default.o2.radiantbluecloud.com/o2/geoscript-app:latest
-oc login https://openshift-master.radiantbluecloud.com:8443
-oc project omar-dev
+docker build -t $APP_NAME:$APP_TAG .
+docker tag $APP_NAME:latest $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
+oc login $OPENSHIFT_URL
+#oc project omar-dev
 docker login -p `oc whoami -t` -e unused -u unused docker-registry-default.o2.radiantbluecloud.com
-docker push docker-registry-default.o2.radiantbluecloud.com/o2/geoscript-app
+docker push  $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
--- apps/service-proxy-app/deploy-image.sh
+++ apps/service-proxy-app/deploy-image.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 APP_NAME=service-proxy
-APP_TAG=release
+APP_TAG=latest
+
 DOCKER_REGISTRY_URL=docker-registry-default.o2.radiantbluecloud.com
 REGISTRY_PROJECT_NAME=o2
 OPENSHIFT_URL=https://openshift-master.radiantbluecloud.com:8443
 
 ./gradlew assemble
-
-docker build -t $APP_NAME:$APP_TAG .
+sed -e s/REGISTRY_URI/$DOCKER_REGISTRY_URL/g -e s/TAG_PLACEHOLDER/$APP_TAG/g Dockerfile > Dockerfile.tmp
+docker build -f Dockerfile.tmp -t $APP_NAME:$APP_TAG .
+rm -f Dockerfile.tmp
 docker tag $APP_NAME:latest $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
 oc login -u $OPENSHIFT_USER -p $OPENSHIFT_PASSWORD $OPENSHIFT_URL
 docker login -p `oc whoami -t` -e unused -u unused $DOCKER_REGISTRY_URL
