c075786065753e6a27deea653d72e2d452823810
melbsurfer <melbsurfer@gmail.com>
2017-02-17 10:45:57 -0500

    Refactored deploy script to use env. vars. to log in to Docker reg.

--- apps/service-proxy-app/deploy-image.sh
+++ apps/service-proxy-app/deploy-image.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 APP_NAME=service-proxy
-APP_TAG=latest
+APP_TAG=release
 DOCKER_REGISTRY_URL=docker-registry-default.o2.radiantbluecloud.com
 REGISTRY_PROJECT_NAME=o2
 OPENSHIFT_URL=https://openshift-master.radiantbluecloud.com:8443
 
 ./gradlew assemble
+
 docker build -t $APP_NAME:$APP_TAG .
 docker tag $APP_NAME:latest $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
-oc login $OPENSHIFT_URL
-#oc project omar-dev
+oc login -u $OPENSHIFT_USER -p $OPENSHIFT_PASSWORD $OPENSHIFT_URL
 docker login -p `oc whoami -t` -e unused -u unused $DOCKER_REGISTRY_URL
-
-CMD="docker push  $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG"
-$CMD
+docker push  $DOCKER_REGISTRY_URL/$REGISTRY_PROJECT_NAME/$APP_NAME:$APP_TAG
