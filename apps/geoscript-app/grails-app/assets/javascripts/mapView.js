65c3e3a3969112b47cd77857353fdbf32b36f163
Scott Bortman <scott.bortman@gmail.com>
2017-02-23 10:32:17 -0500

    Made ref map configurable

--- apps/geoscript-app/grails-app/assets/javascripts/mapView.js
+++ apps/geoscript-app/grails-app/assets/javascripts/mapView.js
@@ -5,39 +5,42 @@
 var MapView = (function(){
   function init(params)
   {
+    console.console.log(params);
+    
     var layers = [
-            new ol.layer.Tile({
-              source: new ol.source.TileWMS({
-                url: 'http://web.dev.o2.ossimc2s.com/service-proxy/wmsProxy',
-                params: {
-                  'LAYERS': 'o2-basemap-basic',
-                  'FORMAT': 'image/jpeg'
-                }
-              })
-            }),
-            new ol.layer.Tile({
-              source: new ol.source.TileWMS({
-                url:  (params.contextPath || '') + '/evwhs/wms',
-                params: {
-                }
-              })
-            })
-          ];
+      new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+          url: params.refMap.url,
+          params: {
+            'LAYERS': params.refMap.layer,
+            'FORMAT': 'image/jpeg'
+          }
+        })
+      }),
+      new ol.layer.Tile({
+        source: new ol.source.TileWMS({
+          url:  (params.contextPath || '') + '/evwhs/wms',
+          params: {
+          }
+        })
+      })
+    ];
 
-          var map = new ol.Map({
-            controls: ol.control.defaults().extend([
-              new ol.control.ScaleLine({
-                units: 'degrees'
-              })
-            ]),
-            layers: layers,
-            target: 'map',
-            view: new ol.View({
-              // projection: 'EPSG:4326',
-              center: [0, 0],
-              zoom: 2
-            })
-          });  }
+    var map = new ol.Map({
+      controls: ol.control.defaults().extend([
+        new ol.control.ScaleLine({
+          units: 'degrees'
+        })
+      ]),
+      layers: layers,
+      target: 'map',
+      view: new ol.View({
+        // projection: 'EPSG:4326',
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
   return {
     init: init
   };
--- apps/geoscript-app/grails-app/controllers/geoscript/app/EvwhsController.groovy
+++ apps/geoscript-app/grails-app/controllers/geoscript/app/EvwhsController.groovy
@@ -1,12 +1,27 @@
 package geoscript.app
 
+import org.springframework.beans.factory.annotation.Value
+
 class EvwhsController
 {
     def evwhsService
 
+    @Value('${geoscript.app.refMap.url}')
+    def refMapURL
+
+    @Value('${geoscript.app.refMap.layer}')
+    def refMapLayer
+
     def index()
     {
-      [mapViewParams: [contextPath: grailsApplication.config.server.contextPath]]
+      [mapViewParams: [
+        contextPath: grailsApplication.config.server.contextPath,
+        refMap: [
+          url: refMapURL,
+          layer: refMapLayer
+          ]
+        ]
+      ]
     }
 
     def wms()
