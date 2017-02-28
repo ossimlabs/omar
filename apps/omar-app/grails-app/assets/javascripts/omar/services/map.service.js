6ba980cb6ba28ce73d55c00877f59c402b91b8c4
Scott Bortman <scott.bortman@gmail.com>
2017-02-28 08:42:33 -0500

    Ensure Footprint styling works for different themes besides File Type,  like Mission or Sensor

--- apps/omar-app/grails-app/assets/javascripts/omar/services/map.service.js
+++ apps/omar-app/grails-app/assets/javascripts/omar/services/map.service.js
@@ -428,10 +428,13 @@ function mapService(stateService, wfsService) {
 
     clearLayerSource(searchLayerVector);
 
+console.log(imageObj.geometry.coordinates);
 
     var footprintFeature = new ol.Feature({
         geometry: new ol.geom.MultiPolygon(imageObj.geometry.coordinates)
     });
+
+
 
     var color = setFootprintColors(imageObj.properties.file_type);
 
@@ -443,6 +446,9 @@ function mapService(stateService, wfsService) {
     searchLayerVector.getSource().addFeature(footprintFeature);
 
     var featureExtent = footprintFeature.getGeometry().getExtent();
+
+    console.log(featureExtent);
+
     var featureExtentCenter = new ol.extent.getCenter(featureExtent);
 
     var missionID = "Unknown";
@@ -632,54 +638,54 @@ function mapService(stateService, wfsService) {
     var color = "rgba(255, 255, 50, 0.6)";
 
     switch (imageType) {
-      case "adrg":
-          color = "rgba(50, 111, 111, 0.6)"; // atoll
-          break;
-      case "aaigrid":
-          color = "rgba(255, 192, 203, 0.6)"; // pink
-          break;
-      case "cadrg":
-          color = "rgba(0, 255, 255, 0.6)"; // cyan
-          break;
-      case "ccf":
-          color = "rgba(128, 100, 255, 0.6)"; // light slate blue
-          break;
-      case "cib":
-          color = "rgba(0, 128, 128, 0.6)"; // teal
-          break;
-      case "doqq":
-          color = "rgba(128, 0, 128, 0.6)"; // purple
-          break;
-      case "dted":
-          color = "rgba(0, 255, 0, 0.6)"; // green
-          break;
-      case "imagine_hfa":
-          color = "rgba(211, 211, 211, 0.6)"; // lightGrey
-          break;
-      case "jpeg":
-          color = "rgba(255, 255, 0, 0.6)"; // yellow
-          break;
-      case "jpeg2000":
-          color = "rgba(255, 200, 0, 0.6)"; // orange
-          break;
-      case "landsat7":
-          color = "rgba(255, 0, 255, 0.6)"; // pink
-          break;
-      case "mrsid":
-          color = "rgba(0, 188, 0, 0.6)"; // light green
-          break;
-      case "nitf":
-          color = "rgba(0, 0, 255, 0.6)"; // blue
-          break;
-      case "tiff":
-          color = "rgba(255, 0, 0, 0.6)"; // red
-          break;
-      case "mpeg":
-          color = "rgba(164, 254, 255, 0.6)"; // red
-          break;
-      case "unspecified":
-          color = "rgba(255, 255, 255, 0.6)"; // white
-          break;
+      // case "adrg":
+      //     color = "rgba(50, 111, 111, 0.6)"; // atoll
+      //     break;
+      // case "aaigrid":
+      //     color = "rgba(255, 192, 203, 0.6)"; // pink
+      //     break;
+      // case "cadrg":
+      //     color = "rgba(0, 255, 255, 0.6)"; // cyan
+      //     break;
+      // case "ccf":
+      //     color = "rgba(128, 100, 255, 0.6)"; // light slate blue
+      //     break;
+      // case "cib":
+      //     color = "rgba(0, 128, 128, 0.6)"; // teal
+      //     break;
+      // case "doqq":
+      //     color = "rgba(128, 0, 128, 0.6)"; // purple
+      //     break;
+      // case "dted":
+      //     color = "rgba(0, 255, 0, 0.6)"; // green
+      //     break;
+      // case "imagine_hfa":
+      //     color = "rgba(211, 211, 211, 0.6)"; // lightGrey
+      //     break;
+      // case "jpeg":
+      //     color = "rgba(255, 255, 0, 0.6)"; // yellow
+      //     break;
+      // case "jpeg2000":
+      //     color = "rgba(255, 200, 0, 0.6)"; // orange
+      //     break;
+      // case "landsat7":
+      //     color = "rgba(255, 0, 255, 0.6)"; // pink
+      //     break;
+      // case "mrsid":
+      //     color = "rgba(0, 188, 0, 0.6)"; // light green
+      //     break;
+      // case "nitf":
+      //     color = "rgba(0, 0, 255, 0.6)"; // blue
+      //     break;
+      // case "tiff":
+      //     color = "rgba(255, 0, 0, 0.6)"; // red
+      //     break;
+      // case "mpeg":
+      //     color = "rgba(164, 254, 255, 0.6)"; // red
+      //     break;
+      // case "unspecified":
+      //     color = "rgba(255, 255, 255, 0.6)"; // white
+      //     break;
       default:
           color = "rgba(255, 255, 255, 0.6)"; // white
 
