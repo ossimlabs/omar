e29546310e3f224c08d0c5745087ceae151693d7
knaquin <knaquin@radiantblue.com>
2017-03-01 08:54:11 -0500

    Adding back in the HTTP Builder code.

--- apps/tlv-app/grails-app/services/time_lapse/HttpDownloadService.groovy
+++ apps/tlv-app/grails-app/services/time_lapse/HttpDownloadService.groovy
@@ -2,9 +2,11 @@ package time_lapse
 
 
 import grails.transaction.Transactional
-import groovy.json.JsonSlurper
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.GET
+import java.security.KeyStore
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.ssl.SSLSocketFactory
 
 
 @Transactional
@@ -12,34 +14,39 @@ class HttpDownloadService {
 
 
 	def serviceMethod(params) {
-		try {
-			def command = "curl -Lk ${ params.url }"
-println command
-			def process = command.execute()
-			process.waitFor()
-			def text = process.getText()
-			def json = new JsonSlurper().parseText( text )
+		def http = new HTTPBuilder( params.url )
 
+		def keyStoreFile = getClass().getResource( "/keyStore.jks" )
+		def trustStoreFile = getClass().getResource( "/trustStore.jks" )
 
-			return json
-			//http.request( GET ) { req ->
-			//	response.failure = { resp, reader ->
-//println "Failure: ${reader}"
+		if ( keyStoreFile && trustStoreFile ) {
+			def keyStore = KeyStore.getInstance( KeyStore.defaultType )
+			keyStoreFile.withInputStream { keyStore.load( it, "tlvCheese".toCharArray() ) }
 
+			def trustStore = KeyStore.getInstance( KeyStore.defaultType )
+			trustStoreFile.withInputStream { trustStore.load( it, "tlvCheese".toCharArray() ) }
 
-//					return null
-//				}
-//				response.success = { resp, reader ->
-
-
-//					return reader
-//				}
-//			}
+			def ssl = new SSLSocketFactory( keyStore, "tlvCheese", trustStore )
+			http.client.connectionManager.schemeRegistry.register( new Scheme( "https", ssl, 443 ) )
 		}
-		catch ( Exception e ) {
+
+		try {
+			http.request( GET ) { req ->
+				response.failure = { resp, reader ->
+					println "Failure: ${reader}"
+
+
+					return null
+				}
+				response.success = { resp, reader ->
+
+
+					return reader
+				}
+			}
+		}
+		catch (Exception e) {
 			println e
-
-
 			return null
 		}
 	}
