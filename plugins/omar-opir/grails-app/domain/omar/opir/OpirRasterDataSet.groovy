5b8ae77e1d80c0da85a769567f4525c86ffc7c24
Garrett Potts <gpotts@radiantblue.com>
2017-03-03 10:33:36 -0500

    Added some initial work to support indexing OPIR sensors.  Still way off from being done but wated to add some simple cases

--- plugins/omar-opir/grails-app/domain/omar/opir/OpirRasterDataSet.groovy
+++ plugins/omar-opir/grails-app/domain/omar/opir/OpirRasterDataSet.groovy
@@ -11,6 +11,33 @@ class OpirRasterDataSet {
       cache true
       id generator: 'identity'
     }
+
+
+    static OpirRasterDataSet initRasterDataSet(rasterDataSetNode, rasterDataSet = null)
+    {
+        rasterDataSet = rasterDataSet ?: new OpirRasterDataSet()
+
+        for ( def rasterFileNode in rasterDataSetNode.fileObjects.RasterFile )
+        {
+            OpirRasterFile rasterFile = OpirRasterFile.initRasterFile( rasterFileNode )
+            rasterDataSet.addToFileObjects( rasterFile )
+        }
+
+        for ( def rasterEntryNode in rasterDataSetNode.rasterEntries.RasterEntry )
+        {
+            OpirRasterEntry rasterEntry = new OpirRasterEntry()
+            rasterEntry.rasterDataSet = rasterDataSet
+            OpirRasterEntry.initRasterEntry( rasterEntryNode, rasterEntry )
+
+            if ( rasterEntry.groundGeom )
+            {
+                rasterDataSet.addToRasterEntries( rasterEntry )
+            }
+        }
+
+        return rasterDataSet
+    }
+
     def getFileFromObjects(def type = "main")
     {
         return fileObjects?.find { it.type == type }
