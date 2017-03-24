ce4ff0440672f80600442e422b35fdcd0d0b4aac
Garrett Potts <gpotts@radiantblue.com>
2017-03-24 11:57:13 -0400

    Added a bridge for the new build system so artifacts still can be produced like the old system

--- build_scripts/linux/build.sh
+++ build_scripts/linux/build.sh
@@ -9,6 +9,26 @@ popd >/dev/null
 
 . $SCRIPT_DIR/env.sh
 
+if [ -z $OMAR_COMMON_PROPERTIES ] ; then
+  echo "OMAR_COMMON_PROPERTIES is not defined and must point to the common-properties file"
+  echo "The comon proeprties can be downloaded from https://github.com/ossimlabs/omar-common"
+  exit 1
+fi
+#
+# Compile the plugins first
+#
+for plugin in ${O2_PLUGINS[@]} ; do
+   echo "BUILDING: $plugin ..."
+   pushd $OMAR_DEV_HOME/plugins/$plugin
+    ./gradlew install
+   if [ $? -ne 0 ] ; then
+       echo "BUILD ERROR: ${plugin} failed to build..."
+       popd
+       exit 1
+   fi
+   popd
+done
+
 for app in ${O2_APPS[@]} ; do
    echo "BUILDING: $app ..."
    pushd $OMAR_DEV_HOME/apps/$app
@@ -18,7 +38,7 @@ for app in ${O2_APPS[@]} ; do
       ./gradlew assemble
    fi
    if [ $? -ne 0 ];then
-       echo "BUILD ERROR: $x failed to build..."
+       echo "BUILD ERROR: $app failed to build..."
        exit 1
    fi
    popd
